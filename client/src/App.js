de0259380e8272b010c3db641f8e8525342106bc
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2021-12-01 11:41:04 +0300

    install react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
 import InformationCard from "./component/InformationCard";
 import Registration from "./component/Registration";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <InformationCard /> */}
-      <Registration />
+      <Registration />{" "}
+      <Router>
+        <div>
+          <nav></nav>
+          <Switch>
+            <Route path="/registration">{/* <RegistrationPage /> */}</Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
